2aac2f889f63a1142bf6262385a4c213f70bc998
Jacek Ciesielski <ciesielskijacek@interia.pl>
2019-12-22 00:21:33 +0100

    dasd

---
+++ BookListRazor/BookListRazor/Pages/BookList/CreateNewBook.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor
+{
+    public class CreateNewBookModel : PageModel
+    {
+        private readonly ApplicationDbContext db;
+
+        public CreateNewBookModel(ApplicationDbContext aDb)
+        {
+            db = aDb;
+        }
+
+        public Book Book { get; set; }
+            
+        public void OnGet()
+        {
+
+        }
+    }
+}
